287bf2ef4e060eedca967bc5e57cdcb08917f55d
Franco_Ojeda <24martin94@gmail.com>
2024-08-03 23:20:03 -0300

    Inicio de nuevo proyecto. Grilla de productos

---
+++ projectpage/src/lib/dataProducts.js
@@ -0,0 +1,66 @@
+export const products = [
+  {
+    id: 1,
+    name: "Waffle with Berries",
+    description: "Waffle",
+    price: 6.50,
+    image: "./challengeAssets/images/product-list/image-waffle-desktop.jpg"
+  },
+  {
+    id: 2,
+    name: "Pistachio Baklava",
+    description: "Baklava",
+    price: 4.00,
+    image: "./challengeAssets/images/product-list/image-baklava-desktop.jpg"
+  },
+  {
+    id: 3,
+    name: "Salted Caramel Brownie",
+    description: "Brownie",
+    price: 5.50,
+    image: "./challengeAssets/images/product-list/image-brownie-desktop.jpg"
+  },
+  {
+    id: 4,
+    description: "Cake",
+    name: "Red Velvet Cake",
+    price: 4.50,
+    image: "./challengeAssets/images/product-list/image-cake-desktop.jpg"
+  },
+  {
+    id: 5,
+    name: "Vanilla Bean Crème Brûlée",
+    description: "Crème Brûlée",
+    price: 7.00,
+    image: "./challengeAssets/images/product-list/image-creme-brulee-desktop.jpg"
+  },
+  {
+    id: 6,
+    name: "Macaron Mix of Five",
+    description: "Macaron",
+    price: 8.00,
+    image: "./challengeAssets/images/product-list/image-macaron-desktop.jpg"
+  },
+  {
+    id: 7,
+    name: "Lemon Meringue Pie",
+    description: "Pie",
+    price: 5.00,
+    image: "./challengeAssets/images/product-list/image-meringue-desktop.jpg"
+  },
+  {
+    id: 8,
+    name: "Vanilla Panna Cotta",
+    description: "Panna Cotta",
+    price: 6.50,
+    image: "./challengeAssets/images/product-list/image-panna-cotta-desktop.jpg"
+  },
+  {
+    id: 9,
+    name: "Classic Tiramisu",
+    description: "Tiramisu",
+    price: 5.50,
+    image: "./challengeAssets/images/product-list/image-tiramisu-desktop.jpg"
+  },
+
+]
